2b44a2e3d709f56018394fa027cd5db1bba6d04b
MaverickSamar <ms59313@gmail.com>
2023-03-16 20:47:25 +0530

    Auth pages added

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,8 +1,78 @@
-import React from 'react'
+import React from 'react';
+import {Container, Row, Col, Form, FormGroup} from "reactstrap";
+import Helmet from '../components/Helmet/Helmet';
+import CommonSection from '../components/UI/CommonSection';
+import '../styles/checkout.css';
+import {useSelector} from "react-redux";
+
 
 const Checkout = () => {
+
+
+  const totalQty = useSelector(state=> state.cart.totalQuantity)
+  const totalAmount = useSelector(state=> state.cart.totalAmount)
   return (
-    <div>Checkout</div>
+    <Helmet title="Checkout">
+      <CommonSection title="Checkout"/>
+      <section> 
+        <Container>
+          <Row>
+            <Col lg='8'>
+              <h6 className="fw-bold mb-4">
+                Billing Information
+              </h6>
+
+              <Form className='billing__form'>
+                <FormGroup className="form__group">
+                  <input type="text" placeholder='Enter your name' required/>
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input type="email" placeholder='Enter your email' required/>
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input type="number" placeholder='Phone number' required/>
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input type="text" placeholder='Address' required/>
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input type="text" placeholder='City' required/>
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input type="text" placeholder='City' required/>
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input type="number" placeholder='Pincode' required/>
+                </FormGroup>
+              </Form>
+            </Col>
+
+            <Col lg='4'>
+              <div className="checkout__cart">
+                <h6>Total Qty: <span>₹{totalQty}</span></h6>
+                <h6>Subtotal: <span>₹{totalAmount}</span></h6>
+                <h6><span>
+                  Shipping: <br/>
+                  free shipping
+                  </span>
+                  <span>₹0</span>
+                </h6>
+                <h4>Total Cost: <span>₹{totalAmount}</span></h4>
+                <button className="buy__btn auth__btn w-100">
+                  Place Order
+                </button>
+              </div>
+
+                <button className="in__btn auth__btn w-100">
+                  Go to Cart
+                </button>
+            
+            </Col>
+
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
   )
 }
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, {useState} from 'react';
+import Helmet from '../components/Helmet/Helmet';
+import { Container, Row, Col, Form, FormGroup } from 'reactstrap';
+import {Link} from 'react-router-dom';
+import '../styles/login.css';
 
 const Signup = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phonenumber, setPhonenumber] = useState();
+  const [file, setFile] = useState(null);
+
+
   return (
-    <div>Signup</div>
+    <Helmet title='Signup'>
+      <section>
+        <Container>
+          <Row>
+            <Col lg='6' className="text-center m-auto">
+              <h3 className="fw-bold mb-4">
+                Login
+              </h3>
+
+              <Form className="auth__form">
+                <FormGroup className="form__group">
+                  <input type="text" placeholder='Enter your name' value={username} onChange={e=>setUsername(e.target.value)}/>
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input type="email" placeholder='Enter your email' value={email} onChange={e=>setEmail(e.target.value)}/>
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input type="password" placeholder='Enter your password' value={password} onChange={e=>setPassword(e.target.value)}/>
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input type="number" placeholder='Enter your phone number' value={phonenumber} onChange={e=>setPhonenumber(e.target.value)}/>
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input type="file" placeholder='Choose avatar' value={file} onChange={e=>setFile(e.target.file)}/>
+                </FormGroup>
+
+                <button type='submit' className="buy__btn auth__btn">
+                  Create an account
+                </button>
+                <p>Already have an account? <Link to='/login'>Login</Link></p>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
   )
 }
 
