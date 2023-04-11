77741638235ec3565783014a5a6145d51f6dc71f
MaverickSamar <ms59313@gmail.com>
2023-04-12 04:31:35 +0530

    bugs fixed

--- src/components/UI/BlogCommon.jsx
+++ src/components/UI/BlogCommon.jsx
@@ -4,7 +4,7 @@ import '../../styles/blog-common.css'
 const CommonSection = ({title}) => 
 {
   return (
-    <section className="common__section">
+    <section className="common__section-blog">
         <Container className='text-center'>
             <h1>{title}</h1>
         </Container>
